4505effb2f3621d1a2defa3a2b64f222f66d04bc
Valentino <devalen543@gmail.com>
2024-12-27 15:00:55 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,15 @@ module.exports = {
       boxShadow: { 'myshadow': '0 4px 15px rgba(0, 0, 0, 0.4)' },
       maxWidth: { '1/2': '50%' },
       screens: {
-        'celu': { min: '425px', /*falta max*/ },  // Para pantallas de hasta 425px
         'celu-s': { min: '320px', max: '425px' },  // Para el rango entre 320px y 425px
+        'celu': { min: '426px', max: '639px'},
+        /*
+        sm	640px	
+        md	768px	
+        lg	1024px	
+        xl	1280px	
+        2xl	1536px 
+        */
       },
     },
   },
